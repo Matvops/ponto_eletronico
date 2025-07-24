d446146507c2d7f70502cddfdc8ed78ff5294971
Matheus <matheusmartinscia@gmail.com>
2025-07-24 18:35:52 -0300

    Feat: Cria controller para Autorização

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class AuthController extends Controller
+{
+    
+
+    public function login(): View
+    {
+        return view('auth.login');
+    }
+}
