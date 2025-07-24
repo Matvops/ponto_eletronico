fef037ebaa563b793ade5041a72dd76da813b4ef
Matheus <matheusmartinscia@gmail.com>
2025-07-24 18:34:37 -0300

    Feat: Cria component layout principal

---
+++ resources/views/components/layouts/main_layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>{{ $title }}</title>
+</head>
+<body>
+    {{ $content }}
+</body>
+</html>
