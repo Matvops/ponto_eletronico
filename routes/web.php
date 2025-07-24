14f1a4f0a48f99856ccc0081951eec0d24b13bb6
Matheus <matheusmartinscia@gmail.com>
2025-07-24 18:36:20 -0300

    Feat: Adiciona route para tela de login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::middleware('guest')->group(function() {
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
 });
