7517dd303928bb6acf19489c519dd73dd6020a76
Matheus <matheusmartinscia@gmail.com>
2025-07-26 18:01:13 -0300

    Feat: Adiciona html do botão e corrige css

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,8 +1,7 @@
 <div class="flex">
-    <form action="" method="POST" class="px-6 mt-12 text-shadow-sm flex flex-col w-[30%]">
+    <form action="" method="POST" class="px-6 mt-12 text-shadow-sm flex flex-col w-[30%] justify-between">
         @csrf
-        <legend class="text-white font-inter font-semibold tracking-wide text-shadow-lg text-6xl flex-1">Login
-        </legend>
+        <legend class="text-white font-inter font-semibold tracking-wide text-shadow-lg text-6xl">Login</legend>
         <div class="my-32 flex flex-col gap-12">
             <label class="flex flex-col text-white text-4xl font-medium text-shadow-md">
                 Email
@@ -16,7 +15,11 @@
                     class="mt-2 background-secondary-color py-3 px-2 outline-0 inset-shadow-gray-700 focus:inset-shadow-xs focus:px-3 transition-all duration-100 ease-in-out text-black text-xl font-light rounded">
             </label>
         </div>
-        <livewire:button text="Entrar" />
+        <button
+            type="submit"
+            class="bg-white hover:bg-gray-100 text-3xl text-gray-800 font-semibold py-3 px-4 border border-gray-400 rounded shadow cursor-pointer font-inter mb-10 mx-auto">
+        Entrar
+        </button>
     </form>
     <div class="background-primary-color w-[70%] h-screen m-0 p-0 ml-auto text-center h-screen">
         <h1 class="text-white font-bold text-8xl font-inter m-0 mt-32 tracking-wider text-shadow-lg">Ponto Prati
