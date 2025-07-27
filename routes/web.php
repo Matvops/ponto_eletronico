04fcf8e5bb641937341a0b7626a0ad60e21f6ded
Matheus <matheusmartinscia@gmail.com>
2025-07-27 14:59:22 -0300

    Feat: Altera route para view de login e cria route para auth

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Livewire\Auth\Login;
 use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('guest')->group(function() {
-    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::get('/login', Login::class)->name('login');
+    Route::post('/login', [AuthController::class, 'authentication'])->name('authentication');
 });
