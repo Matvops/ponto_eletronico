7a1507260af447e7a51ff80d3680f93ce3016fbe
Matheus <matheusmartinscia@gmail.com>
2025-07-30 16:42:43 -0300

    Feat: Cria service para enviar email

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -2,12 +2,13 @@
 
 namespace App\Services;
 
+use App\Mail\ConfirmationCode;
 use App\Models\User;
 use App\Utils\Response;
 use Exception;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 
 class AuthService {
     
@@ -35,13 +36,51 @@ class AuthService {
 
         if(!$user->email_verified_at) throw new Exception("Verifique seu cadastro no email");
 
-        error_log($user->password);
-        error_log($password);
-
         if(!password_verify($password, $user->password)) throw new Exception("Email ou senha são inválidos");
 
         Auth::login($user);
 
         session()->regenerate();
+    }
+
+    public function sendEmailConfirmation($email): Response
+    {
+
+        try {
+            DB::beginTransaction();
+
+            $user = User::where('email', $email)
+                        ->whereNull('deleted_at')
+                        ->first();
+
+            if(!$user->email_verified_at) throw new Exception("Verifique seu cadastro no email");
+
+            $user->confirmation_code = $this->generateRandomConfirmationCode();
+            $user->save();    
+            
+            $this->send($user);
+
+            DB::commit();
+            return Response::getResponse(true, data: $user->email);
+        } catch (Exception $e) {
+            DB::rollBack();
+            error_log($e->getMessage());
+            return Response::getResponse(false, $e->getMessage());
+        }
+    }
+
+    private function generateRandomConfirmationCode(): int
+    {
+        $random = rand(1, 9999);
+
+        $code = str_pad(strval($random), 4, '0', STR_PAD_RIGHT);
+
+        return intval($code);
+    }
+
+    private function send($user) {
+        $response =  Mail::to($user->email)->send(new ConfirmationCode($user->confirmation_code, $user->username));
+
+        if(!$response) throw new Exception("Erro no envio para email de confirmação");
     }
 }
