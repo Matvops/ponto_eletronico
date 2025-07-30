efaacb043b01d2268a67a439b878342e400150f6
Matheus <matheusmartinscia@gmail.com>
2025-07-29 22:10:08 -0300

    Feat: Cria page de esqueci minha senha

---
+++ app/Livewire/ForgotPassword.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Services\AuthService;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Validate;
+use Livewire\Component;
+
+class ForgotPassword extends Component
+{
+    #[Validate(rule:'required', message:'obrigatório')]
+    #[Validate(rule:'email', message:'inválido')]
+    #[Validate(rule:'exists:users,email', message:'inválido')]
+    public string $email;
+
+    public function mount() {
+        $this->email = '';
+    }
+
+    public function submit() {
+        $this->validate();
+
+        $this->email;
+
+        $service = new AuthService;
+
+        $service->sendEmailConfirmation($this->email);
+
+        return redirect()->route('confirm_code', ['email' => $this->email]);
+    }
+
+    #[Title('Recuperar senha')]
+    public function render()
+    {
+        return view('livewire.forgot-password');
+    }
+}
---
+++ resources/views/livewire/forgot-password.blade.php
@@ -0,0 +1,40 @@
+<div class="flex justify-center items-center h-full">
+    <div class="w-[40%] h-50 w-max-500 background-secondary-color rounded">
+        <p class="mt-6 text-2xl font-inter text-center">Digite o email:</p>
+
+        <form wire:submit="submit" class="mr-auto px-5 pt-10">
+            <input wire:model="email" type="email" class="text-xl bg-white py-1 rounded outline-0 px-1 focus:px-3 transition-all duration- ease-in-out w-full">
+            <div class="flex justify-between my-4">
+                <a href="{{route('login')}}" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow cursor-pointer">
+                Voltar
+                </a>
+
+                @error('email')
+                    <span 
+                        class="text-red-600 text-center m-0 p-0 font-inter self-center"
+                        x-data="{show:true}"
+                        x-show="show"
+                        x-init="setTimeout(() => show = false, 5000)"
+                    >
+                    {{ $message }}
+                    </span>
+                @enderror
+
+                @if(session('error_send_email'))
+                    <span 
+                        class="text-red-600 text-center m-0 p-0 font-inter self-center"
+                        x-data="{show:true}"
+                        x-show="show"
+                        x-init="setTimeout(() => show = false, 5000)"
+                    >
+                    {{ session('error_send_email') }}
+                    </span>
+                @endif
+
+                <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow cursor-pointer">
+                    Enviar código de confirmação
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
