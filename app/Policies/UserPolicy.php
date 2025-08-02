cba45fe9b82c1e59369bd3b2e60520c13361bad4
Matheus <matheusmartinscia@gmail.com>
2025-08-01 21:57:34 -0300

    Feat: Cria policies para ações com usuários

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class UserPolicy
+{
+   public function viewAny(User $userActing): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $manipulatedUser, User $userActing): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $userActing): bool
+    {
+        return $userActing->role === 'ADMIN';
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $manipulatedUser, User $userActing): bool
+    {
+        return $manipulatedUser->role != 'ADMIN' && $userActing->role == 'ADMIN';
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $manipulatedUser, User $userActing): bool
+    {
+        return $manipulatedUser->role != 'ADMIN' && $userActing->role == 'ADMIN';
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $manipulatedUser, User $userActing): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $manipulatedUser, User $userActing): bool
+    {
+        return false;
+    }
+
+    public function viewAll(User $userActing): bool
+    {
+        return $userActing->role == 'ADMIN';
+    }
+
+    public function viewHomeAdmin(User $userActing): bool
+    {
+        return $userActing->role == 'ADMIN';
+    }
+}
