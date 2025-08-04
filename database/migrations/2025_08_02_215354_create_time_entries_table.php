fc869a811049dce9b7a33ceba06cfef3861833d3
Matheus <matheusmartinscia@gmail.com>
2025-08-04 18:51:11 -0300

    Feat: Cria tabela para salvar ponto

---
+++ database/migrations/2025_08_02_215354_create_time_entries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('time_entries', function (Blueprint $table) {
+            $table->id('tie_id')->autoIncrement();
+            $table->foreignId('tie_ent_id')
+                    ->references('ent_id')
+                    ->on('entry_types');
+            $table->foreignId('tie_usr_id')
+                    ->references('usr_id')
+                    ->on('users');
+            $table->timestamp('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('time_entries');
+    }
+};
