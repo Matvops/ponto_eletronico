b2a0a7a77be34e0455a532c08f5ccbb6418ede4d
Matheus <matheusmartinscia@gmail.com>
2025-08-06 18:31:05 -0300

    Feat: Adiciona modal a tela home admin e user

--- app/Livewire/HomeAdmin.php
+++ app/Livewire/HomeAdmin.php
@@ -9,6 +9,17 @@ use Livewire\Component;
 class HomeAdmin extends Component
 {
 
+    public function mount(){
+        if(session()->has('success_update_profile')) {
+            $this->dispatch('exibirModal',
+                "Sucesso!",
+                "success",
+                session('success_update_profile'),
+                "center"
+            );
+        }
+    }
+
     #[Title('Home')]
     #[Layout('components.layouts.app')]
     public function render()
--- app/Livewire/HomeUser.php
+++ app/Livewire/HomeUser.php
@@ -8,6 +8,18 @@ use Livewire\Component;
 
 class HomeUser extends Component
 {
+    
+    public function mount(){
+        if(session()->has('success_update_profile')) {
+            $this->dispatch('exibirModal',
+                "Sucesso!",
+                "success",
+                session('success_update_profile'),
+                "center"
+            );
+        }
+    }
+
 
     #[Title('Home')]
     #[Layout('components.layouts.app')]
