2196b417f4bb0e9afae38b42692d24ed71cbe07b
Matheus <matheusmartinscia@gmail.com>
2025-08-11 22:06:16 -0300

    Feat: Cria controller para registrar novo usuário

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterNewUserRequest;
 use App\Http\Requests\UpdateProfileRequest;
 use App\Services\UserService;
 use Illuminate\Http\RedirectResponse;
@@ -44,5 +45,29 @@ class UserController extends Controller
 
         return redirect()->route('home_admin')
                             ->with('success_update_profile',  $response->getMessage());
+    }
+
+    public function register(RegisterNewUserRequest $request): RedirectResponse
+    {
+        $userData = [
+            'username' => $request->input('username'),
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+            'role' => $request->input('role'),
+        ];
+
+        $response = $this->service->register($userData);
+
+        if(!$response->getStatus()) {
+            return back()->withInput()
+                            ->with('error_register_user', $response->getMessage());
+        }
+
+        if(Gate::denies('viewHomeAdmin', Auth::user()))
+            return redirect()->route('home_user')
+                            ->with('success_register_user',  $response->getMessage());
+
+        return redirect()->route('home_admin')
+                            ->with('success_register_user',  $response->getMessage());
     }
 }
