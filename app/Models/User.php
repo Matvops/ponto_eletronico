6e2fd5b2582988c1ab7ec8d1b1dd953fa9d32909
Matheus <matheusmartinscia@gmail.com>
2025-08-18 23:04:03 -0300

    Feat: Adiciona softDelete

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as AuthUser;
 
 class User extends AuthUser
 {
+
+    use SoftDeletes;
+    
     public $table = 'users';
     public $primaryKey = 'usr_id';
     
