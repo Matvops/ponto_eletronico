a4ebb905422cb6a6d688e755355716cd0826fb89
Matheus <matheusmartinscia@gmail.com>
2025-08-19 16:23:13 -0300

    Feat: Remove relação com entryType

--- app/Models/TimeEntry.php
+++ app/Models/TimeEntry.php
@@ -11,17 +11,12 @@ class TimeEntry extends Model
     public $primaryKey = 'tie_id';
 
     public $fillable = [
-        'tie_ent_id',
         'tie_usr_id',
         'date',
+        'type',
         'created_at',
         'updated_at'
     ];
-
-    public function entryType(): BelongsTo
-    {
-        return $this->belongsTo(EntryType::class, 'tie_ent_id');
-    }
 
     public function user(): BelongsTo
     {
