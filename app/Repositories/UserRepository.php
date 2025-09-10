141744b772bfdfdac5955627fbcd6b182a84d736
Matheus <matheusmartinscia@gmail.com>
2025-09-10 19:28:45 -0300

    Feat: Cria UserRepository

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+
+class UserRepository {
+
+
+    public function emailExists($email)
+    {
+        return User::withTrashed()
+                ->where('email', $email)
+                ->exists();
+    }
+
+    public function getByEmail($email)
+    {
+        return User::withTrashed()
+                ->where('email', $email)
+                ->first();
+    }
+}
