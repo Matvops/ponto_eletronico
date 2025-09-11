6f9d4cdde45b9608c251a7d9e739f2cd31e88657
Matheus <matheusmartinscia@gmail.com>
2025-09-11 18:52:32 -0300

    Feat: Remove método estático para resetar saldo de horas

--- app/Services/TimeSheetService.php
+++ app/Services/TimeSheetService.php
@@ -125,11 +125,4 @@ class TimeSheetService {
     {
         return $time >= 0;
     }
-
-    public static function updateTimeSheetStatus(int $usr_id){
-
-        TimeSheet::where('tis_usr_id', $usr_id)
-                    ->where('status', TimeSheetStatus::ATIVO)
-                    ->update(['status' => TimeSheetStatus::INATIVO]);
-    }
 }
