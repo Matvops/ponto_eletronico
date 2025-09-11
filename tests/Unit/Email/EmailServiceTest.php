f61e5a1031713e548c7bb5eb22cfeb5de76b7bf9
Matheus <matheusmartinscia@gmail.com>
2025-09-11 19:26:29 -0300

    Feat: Cria testes para o método send

---
+++ tests/Unit/Email/EmailServiceTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Unit\Email;
+
+use App\Mail\VerifyEmail;
+use App\Services\EmailService;
+use Exception;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\PendingMail;
+use Illuminate\Support\Facades\Mail;
+use Mockery;
+use PHPUnit\Framework\Attributes\RunClassInSeparateProcess;
+use PHPUnit\Framework\TestCase;
+
+#[RunClassInSeparateProcess]
+final class EmailServiceTest extends TestCase {
+
+    private EmailService $emailService;
+    private Mailable $mailable;
+    private $email;
+
+    protected function setUp(): void
+    {
+        $this->email = 'user@example.com';
+        $this->mailable = Mockery::mock(VerifyEmail::class);
+        $this->emailService = new EmailService();
+    }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    public function test_send_email_with_error_invalid_email(): void
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Falha ao enviar email');
+        $this->emailService->send();
+    }
+
+    public function test_send_email_with_error_to_send_email(): void
+    {
+
+        $pendingMailMock = Mockery::mock(PendingMail::class);
+
+        $pendingMailMock->shouldReceive('send')
+                            ->with($this->mailable)
+                            ->andReturnNull();
+        Mail::expects('to')
+                ->with($this->email)
+                ->andReturn($pendingMailMock);
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Falha ao enviar email');
+
+        $this->emailService->setMailStructure($this->email, $this->mailable);
+        $this->emailService->send();
+    }
+}
