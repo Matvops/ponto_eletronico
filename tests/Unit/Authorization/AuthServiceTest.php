32293f993a62d606ca882caebe46dfadee5431d7
Matheus <matheusmartinscia@gmail.com>
2025-09-13 18:11:45 -0300

    Feat: Cria testes para método sendEmailConfirmation

--- tests/Unit/Authorization/AuthServiceTest.php
+++ tests/Unit/Authorization/AuthServiceTest.php
@@ -7,7 +7,9 @@ use App\Repositories\UserRepository;
 use App\Services\AuthService;
 use App\Services\EmailService;
 use App\Utils\Functions;
+use Exception;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Mockery;
 use Mockery\MockInterface;
 use PHPUnit\Framework\Attributes\RunClassInSeparateProcess;
@@ -119,6 +121,140 @@ class AuthServiceTest extends TestCase {
         $this->assertSame("Email ou senha são inválidos", $response->getMessage());
     }
     
+    public function test_send_email_confirmation_successfully(): void
+    {
+
+        DB::expects('beginTransaction')
+                ->andReturnSelf();
+
+        DB::expects('commit')
+            ->andReturnSelf();
+
+        $maxDigits = 4;
+        $randomCode = 1234;
+        $functionsMock = Mockery::mock('alias:' . Functions::class);
+        $functionsMock->shouldReceive('generateRandomCode')
+                        ->with($maxDigits)
+                        ->andReturn($randomCode);
+
+        $userMock = Mockery::mock(User::class);
+        
+        $userMock->shouldReceive('save')->andReturnSelf();
+                        
+        $email = 'user@example.com';
+        $this->makeGetAttributeUser('email_verified_at', $userMock, '2025-08-11 22:04:10.000');
+        $this->makeGetAttributeUser('email', $userMock, $email, $email);
+        $this->makeGetAttributeUser('confirmation_code', $userMock, $randomCode);
+        $this->makeGetAttributeUser('username', $userMock, 'user');
+
+        $this->makeSetAttributeUser('confirmation_code', $userMock, $randomCode);
+
+        $this->userRepositoryStub->method('getOnlyActiveUsersByEmail')
+                                    ->willReturn($userMock);
+
+        $this->emailServiceMock->shouldReceive('setMailStructure')
+                                    ->withAnyArgs()
+                                    ->andReturnSelf();
+
+        $this->emailServiceMock->shouldReceive('send')
+                                ->withAnyArgs()
+                                ->andReturnSelf();
+
+        $response = $this->authService->sendEmailConfirmation($email);
+
+        $this->assertTrue($response->getStatus());
+        $this->assertSame($email, $response->getData());
+    }
+
+    public function test_send_email_confirmation_with_error_invalid_email(): void
+    {
+
+        DB::expects('beginTransaction')
+                ->andReturnSelf();
+
+        DB::expects('rollback')
+            ->andReturnSelf();
+
+
+        $this->userRepositoryStub->method('getOnlyActiveUsersByEmail')
+                                    ->willReturn(null);
+
+        $email = 'user@example.com';
+        $response = $this->authService->sendEmailConfirmation($email);
+
+        $this->assertFalse($response->getStatus());
+        $this->assertSame("Erro ao enviar email", $response->getMessage());
+    }
+
+    public function test_send_email_confirmation_with_error_email_not_verified(): void
+    {
+
+        DB::expects('beginTransaction')
+                ->andReturnSelf();
+
+        DB::expects('rollback')
+            ->andReturnSelf();
+
+        $userMock = Mockery::mock(User::class);
+        
+        $userMock->shouldReceive('save')->andReturnSelf();
+                        
+        $email = 'user@example.com';
+        $this->makeGetAttributeUser('email_verified_at', $userMock, null);
+        $this->makeGetAttributeUser('email', $userMock, $email);
+
+        $this->userRepositoryStub->method('getOnlyActiveUsersByEmail')
+                                    ->willReturn($userMock);
+
+        $response = $this->authService->sendEmailConfirmation($email);
+
+        $this->assertFalse($response->getStatus());
+        $this->assertSame("Verifique seu cadastro no email", $response->getMessage());
+    }
+
+    public function test_send_email_confirmation_with_error_send_email(): void
+    {
+        DB::expects('beginTransaction')
+                ->andReturnSelf();
+
+        DB::expects('rollback')
+            ->andReturnSelf();
+
+        $maxDigits = 4;
+        $randomCode = 1234;
+        $functionsMock = Mockery::mock('alias:' . Functions::class);
+        $functionsMock->shouldReceive('generateRandomCode')
+                        ->with($maxDigits)
+                        ->andReturn($randomCode);
+
+        $userMock = Mockery::mock(User::class);
+        
+        $userMock->shouldReceive('save')->andReturnSelf();
+                        
+        $email = 'user@example.com';
+        $this->makeGetAttributeUser('email_verified_at', $userMock, '2025-08-11 22:04:10.000');
+        $this->makeGetAttributeUser('email', $userMock, $email, $email);
+        $this->makeGetAttributeUser('confirmation_code', $userMock, $randomCode);
+        $this->makeGetAttributeUser('username', $userMock, 'user');
+
+        $this->makeSetAttributeUser('confirmation_code', $userMock, $randomCode);
+
+        $this->userRepositoryStub->method('getOnlyActiveUsersByEmail')
+                                    ->willReturn($userMock);
+
+        $this->emailServiceMock->shouldReceive('setMailStructure')
+                                    ->withAnyArgs()
+                                    ->andReturnSelf();
+
+        $this->emailServiceMock->shouldReceive('send')
+                                ->withAnyArgs()
+                                ->andThrow(Exception::class, "Falha ao enviar email");
+
+        $response = $this->authService->sendEmailConfirmation($email);
+
+        $this->assertFalse($response->getStatus());
+        $this->assertSame("Falha ao enviar email", $response->getMessage());
+    }
 
     private function makeGetAttributeUser(string $attribute, User&MockInterface $modelMock, ...$values): void
     {
